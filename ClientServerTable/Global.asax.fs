d5162daffdc7d673da64bfc7c653bfd1a5fafece
giuliohome <giuliohome@gmail.com>
2019-07-11 11:57:06 +0200

    CSRF token validation: answer https://stackoverflow.com/a/50025477 is still needed for latest firefox (and IE11) to run the websharper "say hi to the server" demo from my dev server

--- ClientServerTable/Global.asax.fs
+++ ClientServerTable/Global.asax.fs
@@ -1,7 +1,32 @@
 namespace ClientServerTable
 
+open System.Web
+
 type Global() =
     inherit System.Web.HttpApplication()
 
     member g.Application_Start(sender: obj, args: System.EventArgs) =
         ()
+
+    member __.Application_BeginRequest(sender: obj, args: System.EventArgs) =
+        HttpContext.Current
+        |> function
+        | null -> ()
+        | ctx ->
+            match ctx.Request with
+            | null -> ()
+            | req ->
+                match req.Cookies.Item "csrftoken", req.Headers.Item "x-csrftoken" with
+                | null, null -> ()
+                | cookie, null ->
+                    // fix for IE11, which does not always set the HTTP Header "x-csrftoken"
+                    try req.Headers.Item "x-csrftoken" <- cookie.Value
+                    with _ -> ()       // ignore possible errors
+                | null, _ ->
+                    // if header is set but cookie is not, there's nothing we can do (cookie collection is read-only)
+                    ()
+                | cookie, csrfHeader when cookie.Value <> csrfHeader ->
+                    try req.Headers.Item "x-csrftoken" <- cookie.Value
+                    with _ -> ()       // ignore possible errors
+                | _ ->
+                    ()      // all is fine, the default: cookie "csfrtoken" and header "x-csfrtoken" are equal
